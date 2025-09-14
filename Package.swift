87aa820658d5f7372de2182834ade2ef9519ef76
Ihor Shevchuk <ihorshev@gmail.com>
2025-09-13 21:40:26 -0700

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "piper1-gpl",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "piper1-gpl",
+            targets: ["piper"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/espeak-ng/espeak-ng-spm",
+                revision: "d1a0e917cf23b3f77efa4c9c96549e737cbc0987"
+                 )
+    ],
+    targets: [
+        .target(name: "piper",
+                dependencies: [
+                    .product(name: "libespeak-ng", package: "espeak-ng-spm"),
+                    .target(name: "onnxruntime")
+                ],
+                path: "",
+                sources: [
+                    "piper1-gpl/libpiper/src/piper.cpp"
+                ],
+                publicHeadersPath: "piper1-gpl/libpiper/include",
+                cxxSettings: [
+                    .headerSearchPath("piper-spm/headers")
+                ]
+               ),
+        .binaryTarget(name: "onnxruntime",
+                            url: "https://download.onnxruntime.ai/pod-archive-onnxruntime-c-1.22.0.zip",
+                            // SHA256 checksum
+                            checksum: "90d9de5a139087a6b05a18125d01d01d198820e1731e6f0f11b38749b2ab181f")
+    ],
+    cxxLanguageStandard: .cxx17
+)
+
---
+++ piper-spm/headers/onnxruntime_cxx_api.h
@@ -0,0 +1,8 @@
+//
+//  onnxruntime_cxx_api.h
+//  piper
+//
+//  Created by Ihor Shevchuk on 9/13/25.
+//
+
+#include <onnxruntime/onnxruntime_cxx_api.h>
